fae659077a3bbbbfaab799cc1f18cd8d8b290cbb
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-03 09:32:41 +0500

    tailwind project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/index.html'],
   theme: {
     extend: {
+      margin:{
+        fourside: '8px',
+        boxMargin:'10px'
+      }
     },
   },
   plugins: [],
