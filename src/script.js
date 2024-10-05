541834834da8f7012773a138f2bcf07d1598c723
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-05 21:52:44 +0500

    modified

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
-let btn= document.getElementById("btn");
-let container=document.getElementById("container");
-let go = document.getElementById("go")
-console.log(go)
-btn.addEventListener('click',()=>{
-    go.style.className="name"
- go.innerText="Get Ready"
- btn.style.display="none"
- document.getElementById("counter").style.display="block"
-})
+// let btn= document.getElementById("btn");
+// let container=document.getElementById("container");
+// let go = document.getElementById("go")
+// console.log(go)
+// btn.addEventListener('click',()=>{
+//     go.style.className="name"
+//  go.innerText="Get Ready"
+//  btn.style.display="none"
+//  document.getElementById("counter").style.display="block"
+// })
