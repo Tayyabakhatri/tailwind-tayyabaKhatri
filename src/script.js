bdf9c4e3fb57ff5018050aabfa98d5c756273e86
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-06 23:44:46 +0500

    modified

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
 let btn= document.getElementById("btn");
-let container=document.getElementById("container");
+let span =document.querySelectorAll('#counter span')
+console.log(span)
 let go = document.getElementById("go")
-console.log(go)
 btn.addEventListener('click',()=>{
     go.style.className="name"
  go.innerText="Get Ready"
  btn.style.display="none"
  document.getElementById("counter").style.display="block"
-})
+ span.forEach((num)=>{
+
+ })
+   
+ })
